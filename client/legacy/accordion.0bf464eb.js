d49464c1ad5f66af0dfac5bd05dd6279669e162d
webstz <60821341+webstz@users.noreply.github.com>
2020-02-09 11:18:10 +0700

    Update accordion.0bf464eb.js

--- client/legacy/accordion.0bf464eb.js
+++ client/legacy/accordion.0bf464eb.js
@@ -1,1 +1,1 @@
-import{w as a}from"./_sidebar.aa2a12b9.js";var s=a(!1),r=a(!1),o=a(!1);export{o as i,r as o,s as p};
+import{w as a}from"./sidebar.aa2a12b9.js";var s=a(!1),r=a(!1),o=a(!1);export{o as i,r as o,s as p};
